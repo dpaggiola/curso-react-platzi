7a5eed2ede6730493fc8818d552d63d6a5db2599
Daniel Paggiola <dpaggiola@gmail.com>
2026-07-15 22:46:30 -0300

    Clase 26

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -7,6 +7,7 @@ import { TodosLoading } from '../TodosLoading';
 import { TodosError } from '../TodosError';
 import { EmptyTodos } from '../EmptyTodos';
 import { CreateTodoButton } from '../CreateTodoButton';
+import { TodoForm } from '../TodoForm';
 import { Modal } from '../Modal';
 import { TodoContext } from '../TodoContext';
 
@@ -48,7 +49,7 @@ function AppUI() {
 
       {openModal && (
         <Modal>
-          la funcionalidad de agregar TODO
+          <TodoForm />
         </Modal>
       )}
     </>
